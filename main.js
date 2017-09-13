cefc68df12b7ef5f891c99c81743c8a7ab44bf0b
Andre Robinson <andrerobinson@Andres-MacBook-Pro.local>
2017-09-13 15:55:31 -0400

    added "kings of Hip Hop" tags to html

--- main.js
+++ main.js
@@ -25,6 +25,32 @@ $(() => {
         points: 1000
     },
 ]
+const dirtySouth = [{
+    Question:"who coined trap music?",
+    Answer:"T.I",
+    points: 200
+},
+{
+    Question:"what was the first rap song?",
+    Answer:"Rappers Delight",
+    points: 400
+},
+{
+    Question:"who is the founder of Hip-Hop?",
+    Answer:"Grand Master Flash",
+    points: 600
+},
+{
+    Question:"How many pillars of Hip-Hop are there?",
+    Answer:"3",
+    points: 800
+},
+{
+    Question:"who is the GOAT?",
+    Answer:"50 Cent",
+    points: 1000
+},
+]
 
 let score = 0;
     $('.hist-2').on('click', function(){
@@ -89,63 +115,63 @@ let score = 0;
         }
     });
     $('.ds2').on('click', function(){
-        const y = prompt(history[0].Question);
-        if (y === history[0].Answer) {
+        const y = prompt(dirtySouth[0].Question);
+        if (y === dirtySouth[0].Answer) {
             alert("correct");
-            score += history[0].points;
+            score += dirtySouth[0].points;
             $('.score').html(score);
         } else {
             alert("incorrect")
-            score -= history[0].points;
+            score -= dirtySouth[0].points;
             $('.score').html(score);
         }
     });
 
     $('.ds4').on('click', function() {
-        const y = prompt(history[1].Question);
-        if (y === history[1].Answer) {
+        const y = prompt(dirtySouth[1].Question);
+        if (y === dirtySouth[1].Answer) {
             alert("correct");
-            score += history[1].points;
+            score += dirtySouth[1].points;
             $('.score').html(score);
         } else {
             alert("incorrect")
-            score -= history[1].points;
+            score -= dirtySouth[1].points;
             $('.score').html(score);
         }
     });
     $('.ds6').on('click', function() {
-        const y = prompt(history[2].Question);
-        if (y === history[2].Answer) {
+        const y = prompt(dirtySouth[2].Question);
+        if (y === dirtySouth[2].Answer) {
             alert("correct");
-            score += history[2].points;
+            score += dirtySouth[2].points;
             $('.score').html(score);
         } else {
             alert("incorrect")
-            score -= history[2].points;
+            score -= dirtySouth[2].points;
             $('.score').html(score);
         }
     });
     $('.ds8').on('click', function() {
-        const y = prompt(history[3].Question);
-        if (y === history[3].Answer) {
+        const y = prompt(dirtySouth[3].Question);
+        if (y === dirtySouth[3].Answer) {
             alert("correct");
-            score += history[3].points;
+            score += dirtySouth[3].points;
             $('.score').html(score);
         } else {
             alert("incorrect")
-            score -= history[3].points;
+            score -= dirtySouth[3].points;
             $('.score').html(score);
         }
     });
     $('.ds10').on('click', function() {
-        const y = prompt(history[4].Question);
-        if (y === history[4].Answer) {
+        const y = prompt(dirtySouth[4].Question);
+        if (y === dirtySouth[4].Answer) {
             alert("correct");
-            score += history[4].points;
+            score += dirtySouth[4].points;
             $('.score').html(score);
         } else {
             alert("incorrect")
-            score -= history[4].points;
+            score -= dirtySouth[4].points;
             $('.score').html(score);
         }
     });
