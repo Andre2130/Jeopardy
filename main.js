98258e1b09d91bf21fdb20e49ca2aaabec04cae2
Andre Robinson <andrerobinson@Andres-MacBook-Pro.local>
2017-09-15 10:55:11 -0400

    card only turns black when correct answer is given

--- main.js
+++ main.js
@@ -163,13 +163,12 @@ let score = 0;
             alert("correct");
             score += history[0].points;
             $('.score').html(score);
+            $(".hist-2").css("background-color", "black");
         } else {
             alert("incorrect")
             score -= history[0].points;
             $('.score').html(score);
         }
-        
-        $(".hist-2").css("background-color", "black");
     });
 
     $('.hist-4').on('click', function() {
@@ -178,12 +177,12 @@ let score = 0;
             alert("correct");
             score += history[1].points;
             $('.score').html(score);
+            $(".hist-4").css("background-color", "black");
         } else {
             alert("incorrect")
             score -= history[1].points;
             $('.score').html(score);
         }
-        $(".hist-4").css("background-color", "black");
     });
     $('.hist-6').on('click', function() {
         const y = prompt(history[2].Question);
@@ -191,12 +190,13 @@ let score = 0;
             alert("correct");
             score += history[2].points;
             $('.score').html(score);
+            $(".hist-6").css("background-color", "black");
         } else {
             alert("incorrect")
             score -= history[2].points;
             $('.score').html(score);
         }
-        $(".hist-6").css("background-color", "black");
+        
     });
     $('.hist-8').on('click', function() {
         const y = prompt(history[3].Question);
@@ -204,12 +204,12 @@ let score = 0;
             alert("correct");
             score += history[3].points;
             $('.score').html(score);
+            $(".hist-8").css("background-color", "black");
         } else {
             alert("incorrect")
             score -= history[3].points;
             $('.score').html(score);
         }
-        $(".hist-8").css("background-color", "black");
     });
     $('.hist-10').on('click', function() {
         const y = prompt(history[4].Question);
@@ -217,12 +217,12 @@ let score = 0;
             alert("correct");
             score += history[4].points;
             $('.score').html(score);
+            $(".hist-10").css("background-color", "black");
         } else {
             alert("incorrect")
             score -= history[4].points;
             $('.score').html(score);
         }
-        $(".hist-10").css("background-color", "black");
     });
     $('.ds2').on('click', function(){
         const y = prompt(dirtySouth[0].Question);
@@ -230,12 +230,12 @@ let score = 0;
             alert("correct");
             score += dirtySouth[0].points;
             $('.score').html(score);
+            $(".ds2").css("background-color", "black");
         } else {
             alert("incorrect")
             score -= dirtySouth[0].points;
             $('.score').html(score);
         }
-        $(".ds2").css("background-color", "black");
     });
 
     $('.ds4').on('click', function() {
@@ -244,12 +244,12 @@ let score = 0;
             alert("correct");
             score += dirtySouth[1].points;
             $('.score').html(score);
+            $(".ds4").css("background-color", "black");
         } else {
             alert("incorrect")
             score -= dirtySouth[1].points;
             $('.score').html(score);
         }
-        $(".ds4").css("background-color", "black");
     });
     $('.ds6').on('click', function() {
         const y = prompt(dirtySouth[2].Question);
@@ -257,12 +257,12 @@ let score = 0;
             alert("correct");
             score += dirtySouth[2].points;
             $('.score').html(score);
+            $(".ds6").css("background-color", "black");
         } else {
             alert("incorrect")
             score -= dirtySouth[2].points;
             $('.score').html(score);
         }
-        $(".ds6").css("background-color", "black");
     });
     $('.ds8').on('click', function() {
         const y = prompt(dirtySouth[3].Question);
@@ -270,12 +270,12 @@ let score = 0;
             alert("correct");
             score += dirtySouth[3].points;
             $('.score').html(score);
+            $(".ds8").css("background-color", "black");
         } else {
             alert("incorrect")
             score -= dirtySouth[3].points;
             $('.score').html(score);
         }
-        $(".ds8").css("background-color", "black");
     });
     $('.ds10').on('click', function() {
         const y = prompt(dirtySouth[4].Question);
@@ -283,12 +283,12 @@ let score = 0;
             alert("correct");
             score += dirtySouth[4].points;
             $('.score').html(score);
+            $(".ds10").css("background-color", "black");
         } else {
             alert("incorrect")
             score -= dirtySouth[4].points;
             $('.score').html(score);
         }
-        $(".ds10").css("background-color", "black");
     });
     $('.koh2').on('click', function(){
         const y = prompt(kingOfHipHop[0].Question);
@@ -296,12 +296,12 @@ let score = 0;
             alert("correct");
             score += kingOfHipHop[0].points;
             $('.score').html(score);
+            $(".koh2").css("background-color", "black");
         } else {
             alert("incorrect")
             score -= kingOfHipHop[0].points;
             $('.score').html(score);
         }
-        $(".koh2").css("background-color", "black");
     });
 
     $('.koh4').on('click', function() {
@@ -310,12 +310,12 @@ let score = 0;
             alert("correct");
             score += kingOfHipHop[1].points;
             $('.score').html(score);
+            $(".koh4").css("background-color", "black");
         } else {
             alert("incorrect")
             score -= kingOfHipHop[1].points;
             $('.score').html(score);
         }
-        $(".koh4").css("background-color", "black");
     });
     $('.koh6').on('click', function() {
         const y = prompt(kingOfHipHop[2].Question);
@@ -323,12 +323,12 @@ let score = 0;
             alert("correct");
             score += kingOfHipHop[2].points;
             $('.score').html(score);
+            $(".koh6").css("background-color", "black");
         } else {
             alert("incorrect")
             score -= kingOfHipHop[2].points;
             $('.score').html(score);
         }
-        $(".koh6").css("background-color", "black");
     });
     $('.koh8').on('click', function() {
         const y = prompt(kingOfHipHop[3].Question);
@@ -336,12 +336,12 @@ let score = 0;
             alert("correct");
             score += kingOfHipHop[3].points;
             $('.score').html(score);
+            $(".koh8").css("background-color", "black");
         } else {
             alert("incorrect")
             score -= kingOfHipHop[3].points;
             $('.score').html(score);
         }
-        $(".koh8").css("background-color", "black");
     });
     $('.koh10').on('click', function() {
         const y = prompt(kingOfHipHop[4].Question);
@@ -349,12 +349,12 @@ let score = 0;
             alert("correct");
             score += kingOfHipHop[4].points;
             $('.score').html(score);
+            $(".koh10").css("background-color", "black");
         } else {
             alert("incorrect")
             score -= kingOfHipHop[4].points;
             $('.score').html(score);
         }
-        $(".koh10").css("background-color", "black");
     });
     $('.qoh2').on('click', function(){
         const y = prompt(queenOfHipHop[0].Question);
@@ -362,12 +362,12 @@ let score = 0;
             alert("correct");
             score += queenOfHipHop[0].points;
             $('.score').html(score);
+            $(".qoh2").css("background-color", "black");
         } else {
             alert("incorrect")
             score -= queenOfHipHop[0].points;
             $('.score').html(score);
         }
-        $(".qoh2").css("background-color", "black");
     });
 
     $('.qoh4').on('click', function() {
@@ -376,12 +376,12 @@ let score = 0;
             alert("correct");
             score += queenOfHipHop[1].points;
             $('.score').html(score);
+            $(".qoh4").css("background-color", "black");
         } else {
             alert("incorrect")
             score -= queenOfHipHop[1].points;
             $('.score').html(score);
         }
-        $(".qoh4").css("background-color", "black");
     });
     $('.qoh6').on('click', function() {
         const y = prompt(queenOfHipHop[2].Question);
@@ -389,12 +389,12 @@ let score = 0;
             alert("correct");
             score += queenOfHipHop[2].points;
             $('.score').html(score);
+            $(".qoh6").css("background-color", "black");
         } else {
             alert("incorrect")
             score -= queenOfHipHop[2].points;
             $('.score').html(score);
         }
-        $(".qoh6").css("background-color", "black");
     });
     $('.qoh8').on('click', function() {
         const y = prompt(queenOfHipHop[3].Question);
@@ -402,12 +402,12 @@ let score = 0;
             alert("correct");
             score += queenOfHipHop[3].points;
             $('.score').html(score);
+            $(".qoh8").css("background-color", "black");
         } else {
             alert("incorrect")
             score -= queenOfHipHop[3].points;
             $('.score').html(score);
         }
-        $(".qoh8").css("background-color", "black");
     });
     $('.qoh10').on('click', function() {
         const y = prompt(queenOfHipHop[4].Question);
@@ -415,12 +415,12 @@ let score = 0;
             alert("correct");
             score += queenOfHipHop[4].points;
             $('.score').html(score);
+            $(".qoh10").css("background-color", "black");
         } else {
             alert("incorrect")
             score -= queenOfHipHop[4].points;
             $('.score').html(score);
         }
-        $(".qoh10").css("background-color", "black");
     });
     $('.wc2').on('click', function(){
         const y = prompt(westCoast[0].Question);
@@ -428,12 +428,12 @@ let score = 0;
             alert("correct");
             score += westCoast[0].points;
             $('.score').html(score);
+            $(".wc2").css("background-color", "black");
         } else {
             alert("incorrect")
             score -= westCoast[0].points;
             $('.score').html(score);
         }
-        $(".wc2").css("background-color", "black");
     });
 
     $('.wc4').on('click', function() {
@@ -442,12 +442,12 @@ let score = 0;
             alert("correct");
             score += westCoast[1].points;
             $('.score').html(score);
+            $(".wc4").css("background-color", "black");
         } else {
             alert("incorrect")
             score -= westCoast[1].points;
             $('.score').html(score);
         }
-        $(".wc4").css("background-color", "black");
     });
     $('.wc6').on('click', function() {
         const y = prompt(westCoast[2].Question);
@@ -455,12 +455,12 @@ let score = 0;
             alert("correct");
             score += westCoast[2].points;
             $('.score').html(score);
+            $(".wc6").css("background-color", "black");
         } else {
             alert("incorrect")
             score -= westCoast[2].points;
             $('.score').html(score);
         }
-        $(".wc6").css("background-color", "black");
     });
     $('.wc8').on('click', function() {
         const y = prompt(westCoast[3].Question);
@@ -468,12 +468,12 @@ let score = 0;
             alert("correct");
             score += westCoast[3].points;
             $('.score').html(score);
+            $(".wc8").css("background-color", "black");
         } else {
             alert("incorrect")
             score -= westCoast[3].points;
             $('.score').html(score);
         }
-        $(".wc8").css("background-color", "black");
     });
     $('.wc10').on('click', function() {
         const y = prompt(westCoast[4].Question);
@@ -481,12 +481,12 @@ let score = 0;
             alert("correct");
             score += westCoast[4].points;
             $('.score').html(score);
+            $(".wc10").css("background-color", "black");
         } else {
             alert("incorrect")
             score -= westCoast[4].points;
             $('.score').html(score);
         }
-        $(".wc10").css("background-color", "black");
     });
     $('.ww2').on('click', function(){
         const y = prompt(worldWide[0].Question);
@@ -494,12 +494,12 @@ let score = 0;
             alert("correct");
             score += worldWide[0].points;
             $('.score').html(score);
+            $(".ww2").css("background-color", "black");
         } else {
             alert("incorrect")
             score -= worldWide[0].points;
             $('.score').html(score);
         }
-        $(".ww2").css("background-color", "black");
     });
 
     $('.ww4').on('click', function() {
@@ -508,12 +508,12 @@ let score = 0;
             alert("correct");
             score += worldWide[1].points;
             $('.score').html(score);
+            $(".ww4").css("background-color", "black");
         } else {
             alert("incorrect")
             score -= worldWide[1].points;
             $('.score').html(score);
         }
-        $(".ww4").css("background-color", "black");
     });
     $('.ww6').on('click', function() {
         const y = prompt(worldWide[2].Question);
@@ -521,12 +521,12 @@ let score = 0;
             alert("correct");
             score += worldWide[2].points;
             $('.score').html(score);
+            $(".ww6").css("background-color", "black");
         } else {
             alert("incorrect")
             score -= worldWide[2].points;
             $('.score').html(score);
         }
-        $(".ww6").css("background-color", "black");
     });
     $('.ww8').on('click', function() {
         const y = prompt(worldWide[3].Question);
@@ -534,12 +534,12 @@ let score = 0;
             alert("correct");
             score += worldWide[3].points;
             $('.score').html(score);
+            $(".ww8").css("background-color", "black");
         } else {
             alert("incorrect")
             score -= worldWide[3].points;
             $('.score').html(score);
         }
-        $(".ww8").css("background-color", "black");
     });
     $('.ww10').on('click', function() {
         const y = prompt(worldWide[4].Question);
@@ -547,13 +547,12 @@ let score = 0;
             alert("correct");
             score += worldWide[4].points;
             $('.score').html(score);
+            $(".ww10").css("background-color", "black");
         } else {
             alert("incorrect")
             score -= worldWide[4].points;
             $('.score').html(score);
         }
-        $(".ww10").css("background-color", "black");
-       
     });
    
 })
